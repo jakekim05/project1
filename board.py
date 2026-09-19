89d5cad3751d75e1abe8e285ca7299c178649acc
KIM JAKE <138196478+jakekim05@users.noreply.github.com>
2026-09-19 14:58:33 +0900

    m

--- board.py
+++ board.py
@@ -2,3 +2,7 @@ class Board:
     def __init__(self, size = 19):
         self.size = size
         self.board = [[-1 for _ in range(size)] for _ in range(size)]
+    def print_board(self):
+        print("   " + " ".join(f"{i:2}" for i in range(self.size)))
+        for i, row in enumerate(self.board):
+            print(f"{i:2} " + " ".join('B' if cell == 0 else 'W' if cell == 1 else '.' for cell in row))
--- gamemanager.py
+++ gamemanager.py
@@ -7,14 +7,15 @@ class GameManager:
         self.player2 = player2
         self.board = Board()
     def whos_win(self):
-        for i in range(19):
-            for j in range(19):
+        for i in range(self.board.size):
+            for j in range(self.board.size):
                 if self.board.board[i][j] != -1:
                     color = self.board.board[i][j]
                     for di, dj in [(1, 0), (0, 1), (1, 1), (1, -1)]:
                         ni, nj = i, j
                         count = 1
-                        while 0 <= ni + di < 19 and 0 <= nj + dj < 19 and self.board.board[ni + di][nj + dj] == color:
+                        while 0 <= ni + di < self.board.size and 0 <= nj + dj < self.board.size\
+                        and self.board.board[ni + di][nj + dj] == color:
                             count += 1
                             ni += di
                             nj += dj
@@ -30,4 +31,12 @@ class GameManager:
             print("White player wins!")
         else:
             print("The game is a draw.")
-    
+
+    def start_turn(self, player, time):
+        move = player.take_turn(self.board.board, time)
+        if move:
+            row, col = move
+            self.board.board[row][col] = player.color
+            print(f"Player {'Black' if player.color == 0 else 'White'} placed at ({row}, {col})")
+        else:
+            print("No valid move made.")
