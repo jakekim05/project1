a2d6723ce39fe5fd0d660137541742615c8b4b39
KIM JAKE <138196478+jakekim05@users.noreply.github.com>
2026-09-19 14:55:47 +0900

    add whos_win, end_game

--- gamemanager.py
+++ gamemanager.py
@@ -11,9 +11,23 @@ class GameManager:
             for j in range(19):
                 if self.board.board[i][j] != -1:
                     color = self.board.board[i][j]
-                    count = 1
                     for di, dj in [(1, 0), (0, 1), (1, 1), (1, -1)]:
                         ni, nj = i, j
                         count = 1
+                        while 0 <= ni + di < 19 and 0 <= nj + dj < 19 and self.board.board[ni + di][nj + dj] == color:
+                            count += 1
+                            ni += di
+                            nj += dj
+                        if count == 5:
+                            return color
+        return -1
 
-    def end_game
+    def end_game(self):
+        winner = self.whos_win()
+        if winner == 0:
+            print("Black player wins!")
+        elif winner == 1:
+            print("White player wins!")
+        else:
+            print("The game is a draw.")
+    
