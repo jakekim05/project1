fee4affa708bba49a6f3b1d5ad6cd0fba3736a45
KIM JAKE <138196478+jakekim05@users.noreply.github.com>
2026-09-19 17:38:22 +0900

    README.txt

--- board.py
+++ board.py
@@ -2,8 +2,10 @@ class Board:
     def __init__(self, size = 19):
         self.size = size
         self.board = [[-1 for _ in range(size)] for _ in range(size)]
+
     def valid_pos(self, row, col):
         return 0 <= row < self.size and 0 <= col < self.size
+    
     def print_board(self):
         print("  " + " ".join(f"{i:2}" for i in range(self.size)))
         for i, row in enumerate(self.board):
