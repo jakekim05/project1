ceb17f3baa5530efda50caf592f345624b4b0b9d
KIM JAKE <138196478+jakekim05@users.noreply.github.com>
2026-09-19 17:15:16 +0900

    remove more smart agent

--- omok.py
+++ omok.py
@@ -3,11 +3,10 @@ from humanplayer import HumanPlayer
 from randomplayer import RandomPlayer
 from simpleplayer import SimplePlayer
 from smartplayergaessapgosu import SmartPlayerGaeSsapGosu
-from godplayernemesis import RealGosu
 
 import sys
 argv = sys.argv
-mkp = [HumanPlayer, RandomPlayer, SimplePlayer, SmartPlayerGaeSsapGosu, RealGosu]
+mkp = [HumanPlayer, RandomPlayer, SimplePlayer, SmartPlayerGaeSsapGosu]
 
 game = GameManager(mkp[int(argv[1])](0), mkp[int(argv[2])](1))
 while game.whos_win() == -1:
