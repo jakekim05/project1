2e7da22d5926cf1c4db44f2ba9286b607c83fd04
KIM JAKE <138196478+jakekim05@users.noreply.github.com>
2026-09-26 16:30:36 +0900

    rmv agents to submit project 1-1

--- README.txt
+++ README.txt
@@ -1,4 +1,4 @@
 (1) 김제이크, 노찬, 임한결
-(2) python3 omok.py 0 3
-(3) smartplayergaessapgosu.py 파일의 모든 내용이 gemini pro 3.1을 통해 생성됨.
+(2) python3 omok.py 0 1 -1
+(3) I did not use AI for this project.
 (4) N/A
--- omok.py
+++ omok.py
@@ -2,12 +2,10 @@ from gamemanager import GameManager
 from humanplayer import HumanPlayer
 from randomplayer import RandomPlayer
 from simpleplayer import SimplePlayer
-from smartplayergaessapgosu import SmartPlayerGaeSsapGosu
-from smartplayerClaude import SmartPlayerClaude
 
 
 def main():
-    mkp = [HumanPlayer, RandomPlayer, SimplePlayer, SmartPlayerGaeSsapGosu, SmartPlayerClaude]
+    mkp = [HumanPlayer, RandomPlayer, SimplePlayer]
     
     wins = 0
     losses = 0
